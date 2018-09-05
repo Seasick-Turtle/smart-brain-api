c17fcdf9246626a2bfa6ff4c44b47b0c8120bf08
Christopher Martin <martincdev@gmail.com>
2018-09-04 19:01:43 -0500

    add get root request & post request for signin

--- server.js
+++ server.js
@@ -2,6 +2,45 @@ const express = require('express');
 
 const app = express();
 
+// const to hold temp database objects
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
+
+// / --> res = this is working
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+// /signing --> POST = success/fail
+
+app.post('/signin', (req, res) => {
+  // verify email and password given match whats in db
+  if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
+});
+
 app.listen(3000, () => {
   console.log('works');
 });
